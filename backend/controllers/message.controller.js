2b35cc29b452b72a5efd73db7f3cfe670c7d9263
manish3-4 <preetisikka553@gmail.com>
2025-11-20 00:17:35 +0530

    sendMessageIssueResolved

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,79 +1,69 @@
 import Conversation from "../models/conversation.model.js";
-import Message  from "../models/message.model.js";
+import Message from "../models/message.model.js";
 import { getRecieverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
-    try {
-        const {message} = req.body;
-        const {id : receiverId} = req.params;
-        const senderId =  req.user._id;
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
 
-        let conversation = await Conversation.findOne({
-            participants: {
-                $all: [senderId, receiverId]
-            }
-        });
+    let conversation = await Conversation.findOne({
+      participants: {
+        $all: [senderId, receiverId],
+      },
+    });
 
-        if(!conversation){
-            conversation =  await Conversation.create({
-                participants: [senderId, receiverId]
-            });
-        }
-
-        let newMessage = new Message({
-            senderId,
-            receiverId,
-            message,
-        });
-
-        if(newMessage){
-            conversation.messages.push(newMessage._id);
-        }
-        //Socket IO FUNCTIONALITY WILL GO HERE
-
-
-        // await conversation.save();
-        // await newMessage.save();
-
-        //this will run parallely
-        await Promise.all([conversation.save(), newMessage.save()]);
-
-        //socket io functionality will go here
-        const recieverSocketId = getRecieverSocketId(receiverId);
-        if(recieverSocketId){
-            io.to(recieverSocketId).emit("newMessage", newMessage);
-        }
-
-        res.status(201).json({newMessage});
-
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
     }
 
-     catch (error) {
-        console.log("ERROR IN SENDMESSAGE",error.message);
-        res.status(500).json({error: "server error in  sending message"});
+    let newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
 
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
     }
-}
+    //this will run parallely
+    await Promise.all([conversation.save(), newMessage.save()]);
 
-export  const getMessage = async (req, res) => {
-    try {
-        
-        const {id : userToChatId} = req.params;
-        const senderId =  req.user._id;
+    // Refresh the message from database to get the createdAt timestamp
+    const savedMessage = await Message.findById(newMessage._id);
 
-        const  conversation = await Conversation.findOne({
-            participants: {
-                $all : [senderId, userToChatId] 
-            }
-        }).populate("messages");
-
-        if(!conversation) return res.status(200).json([]);
-
-        res.status(200).json(conversation.messages);
-
-
-    } catch (error) {
-        console.log("ERROR IN GETMESSAGE",error.message);
-        res.status(500).json({error: "server error in  getting message"});
+    //socket io functionality will go here
+    const recieverSocketId = getRecieverSocketId(receiverId);
+    if (recieverSocketId) {
+      io.to(recieverSocketId).emit("newMessage", savedMessage);
     }
-}
+
+    res.status(201).json(savedMessage);
+  } catch (error) {
+    console.log("ERROR IN SENDMESSAGE", error.message);
+    res.status(500).json({ error: "server error in  sending message" });
+  }
+};
+
+export const getMessage = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: {
+        $all: [senderId, userToChatId],
+      },
+    }).populate("messages");
+
+    if (!conversation) return res.status(200).json([]);
+
+    res.status(200).json(conversation.messages);
+  } catch (error) {
+    console.log("ERROR IN GETMESSAGE", error.message);
+    res.status(500).json({ error: "server error in  getting message" });
+  }
+};
